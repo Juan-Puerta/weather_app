a65c2aa55f9f467872008eab4699faef3414e5ab
Juan Sebastián Puerta Ordoñez <puertasebastianjuan@gmail.com>
2023-01-12 14:38:21 -0500

    Added a description to the weather image

--- src/components/cardCity/CardCity.tsx
+++ src/components/cardCity/CardCity.tsx
@@ -3,16 +3,21 @@ import { RootState } from "../../store/store";
 import { useSelector } from 'react-redux';
 import LabelTemp from '../labelTemp/LabelTemp';
 import InfoCard from '../infoCard/InfoCard';
+import { Popover } from 'antd';
 import "./CardCity.css"
 
 const CardCity: React.FC = () => {
 
     const city = useSelector((state: RootState) => state.city);
 
-    const getHour = (timezone: number): string =>{
+    const getHour = (timezone: number): string => {
         const hour: number = Math.floor(((Date.now() + (timezone * 1000)) / (1000 * 60 * 60)) % 24);
         const minutes: number = Math.floor(((Date.now() + (timezone * 1000)) / (1000 * 60)) % 60);
         return `${ hour }:${ minutes }`;
+    }
+
+    const capitalize = (desc: string): string => {
+        return desc[0].toUpperCase() + desc.slice(1);
     }
 
     return(
@@ -24,10 +29,13 @@ const CardCity: React.FC = () => {
             <hr />
             <div className='card-content'>
                 <div className='div-img'>
-                    <img 
-                        src={`https://openweathermap.org/img/wn/${ city.weather?.icon }@4x.png`} 
-                        alt="" 
-                    />
+                    <Popover trigger="click" content={ capitalize(city.weather.description) }>
+                        <img 
+                            className='weather-img'
+                            src={`https://openweathermap.org/img/wn/${ city.weather?.icon }@4x.png`} 
+                            alt="" 
+                        />
+                    </Popover>
                 </div>
                 <LabelTemp />
             </div>
--- src/models/City.ts
+++ src/models/City.ts
@@ -4,7 +4,7 @@ import { Temperature } from "./Temperature";
 export interface City{
     cod: number;
     city_name?: string;
-    weather?: Weather;
+    weather: Weather;
     temperature: Temperature;
     timezone: number;
 }
