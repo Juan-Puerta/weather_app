efb0f2bea01e94908f7c50cf40c96f4e52ef2030
Juan Sebastián Puerta Ordoñez <puertasebastianjuan@gmail.com>
2023-01-11 19:45:47 -0500

    Added a not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { RootState } from "./store/store";
+import { useSelector } from 'react-redux';
 import { Layout } from 'antd';
 import Menu from "./components/menu/Menu";
 import CardCity from './components/cardCity/CardCity';
+import NotFound from './components/notFound/NotFound';
 import './App.css';
 
 const { Header, Content } = Layout;
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+
+  const city = useSelector((state: RootState) => state.city);
+
+  return(
     <Layout className='app-layout'>
-        <Header style={{ height: 50, }}>
+        <Header style={{ height: 50, background:"#92E3A9"}}>
           <Menu />
         </Header>
         <Content className='content-layout'>
-          <CardCity/>
+          { city.cod === 404 ? <NotFound/> : <CardCity/> }
         </Content>
     </Layout>
-)
+  );
+};
 
 export default App;
